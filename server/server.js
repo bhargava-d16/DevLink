7547a8e3e5283f4b2fb8a3b3885dc00fd90e2b9b
bhargava-d16 <bsdabhade_b23@et.vjti.ac.in>
2025-08-10 22:32:28 +0530

    deploying

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ app.use(express.json({limit:'5mb'}));
 app.use(express.urlencoded({ limit: '5mb', extended: true }));
 app.use(cookieParser());
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: ['http://localhost:5173', 'https://devlink-three-theta.vercel.app'],
   credentials: true
 }));
 
