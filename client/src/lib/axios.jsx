4bef5ce7f59cb7c76afc70f17f2b4ba75b73b0f1
bhargava-d16 <bsdabhade_b23@et.vjti.ac.in>
2025-08-10 22:09:22 +0530

    deploying

--- client/src/lib/axios.jsx
+++ client/src/lib/axios.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance=axios.create({
-    baseURL:"http://localhost:8080/api",
+    baseURL:"https://devlink-7v35.onrender.com",
     withCredentials:true,
 })
